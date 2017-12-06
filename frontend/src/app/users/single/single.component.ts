8c7eb39b314243463a5377ae26a73d3db1ba2ee8
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2017-12-06 00:04:15 -0500

    Added ability to update and extra fields to the user

--- frontend/src/app/users/single/single.component.ts
+++ frontend/src/app/users/single/single.component.ts
@@ -1,7 +1,8 @@
 import { Http, HttpModule, Response } from '@angular/http';
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Location} from '@angular/common';
+import { NgForm } from '@angular/forms';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -16,6 +17,7 @@ export class SingleComponent implements OnInit {
   constructor(
     private http: Http,
     private route: ActivatedRoute,
+    private router: Router,
     private location: Location
   ) {}
 
@@ -31,6 +33,19 @@ export class SingleComponent implements OnInit {
       .subscribe(user => {
         console.log(user);
         this.user = user;
+      });
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f, this.user);
+
+    this.http.put(this.apiUrl + '/users/'+ this.user._id, this.user, {})
+      .map((res: Response) => res.json())
+      .subscribe(user => {
+        console.log('Edit: ', user);
+        this.user = user;
+        window.location.reload();
+        // this.router.navigate(['/users/'+ user._id]);
       });
   }
 }
